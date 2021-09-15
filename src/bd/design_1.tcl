768088b02d114125aaf62c887a8d20332a3df6c8
ArtVVB <arthur.brown@digilent.com>
2021-09-15 15:23:55 -0700

    Validate block design

--- src/bd/design_1.tcl
+++ src/bd/design_1.tcl
@@ -363,6 +363,7 @@ proc create_root_design { parentCell } {
   # Restore current instance
   current_bd_instance $oldCurInst
 
+  validate_bd_design
   save_bd_design
 }
 # End of create_root_design()
@@ -374,6 +375,4 @@ proc create_root_design { parentCell } {
 
 create_root_design ""
 
-
-common::send_msg_id "BD_TCL-1000" "WARNING" "This Tcl script was generated from a block design that has not been validated. It is possible that design <$design_name> may result in errors during validation."
 
