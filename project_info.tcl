41562da56257b8503d0d3d3579745bdca29ef805
Dragos <dragos-ioan.pascu@ni.com>
2022-05-20 16:27:49 +0300

    Incremented board version as in commit 3f67d8f8

--- project_info.tcl
+++ project_info.tcl
@@ -2,7 +2,7 @@
 proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
 	set_property "part" "xc7z020clg484-1" $project_obj
-	set_property "board_part" "digilentinc.com:eclypse-z7:part0:1.0" $project_obj
+	set_property "board_part" "digilentinc.com:eclypse-z7:part0:1.1" $project_obj
 	set_property "default_lib" "xil_defaultlib" $project_obj
 	set_property "simulator_language" "Mixed" $project_obj
 	set_property "target_language" "Verilog" $project_obj
