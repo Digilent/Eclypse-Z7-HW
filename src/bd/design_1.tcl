11d1a03cd35e23992a5172d8cc832f61ac7529d2
Dragos <dragos-ioan.pascu@ni.com>
2022-05-20 16:28:31 +0300

    Block design Vivado 2022.1

--- src/bd/design_1.tcl
+++ src/bd/design_1.tcl
@@ -20,7 +20,7 @@ set script_folder [_tcl::get_script_folder]
 ################################################################
 # Check if script is running in correct Vivado version.
 ################################################################
-set scripts_vivado_version 2021.1
+set scripts_vivado_version 2022.1
 set current_vivado_version [version -short]
 
 if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
@@ -44,7 +44,7 @@ if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
 set list_projs [get_projects -quiet]
 if { $list_projs eq "" } {
    create_project project_1 myproj -part xc7z020clg484-1
-   set_property BOARD_PART digilentinc.com:eclypse-z7:part0:1.0 [current_project]
+   set_property BOARD_PART digilentinc.com:eclypse-z7:part0:1.1 [current_project]
 }
 
 
