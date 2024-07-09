490cf90cf96c0450a53d4dc820248677067d5ccf
DFazakas <denis.fazakas@ni.com>
2024-07-09 17:04:20 +0300

    HW update to 2024.1

--- project_info.tcl
+++ project_info.tcl
@@ -1,11 +1,11 @@
 # This is an automatically generated file used by digilent_vivado_checkout.tcl to set project options
 proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
-	set_property "part" "xc7z020clg484-1" $project_obj
-	set_property "board_part" "digilentinc.com:eclypse-z7:part0:1.0" $project_obj
-	set_property "default_lib" "xil_defaultlib" $project_obj
-	set_property "simulator_language" "Mixed" $project_obj
-	set_property "target_language" "Verilog" $project_obj
+    set_property "part" "xc7z020clg484-1" $project_obj
+    set_property "board_part" "digilentinc.com:eclypse-z7:part0:1.1" $project_obj
+    set_property "default_lib" "xil_defaultlib" $project_obj
+    set_property "simulator_language" "Mixed" $project_obj
+    set_property "target_language" "Verilog" $project_obj
 }
 
 proc set_project_properties_pre_add_repo {proj_name} {
@@ -15,5 +15,10 @@ proc set_project_properties_pre_add_repo {proj_name} {
 
 proc set_project_properties_post_create_runs {proj_name} {
     set project_obj [get_projects $proj_name]
-    # default nothing
+	#Custom directives for synthesis and implementation
+	set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1]
+	set_property STEPS.SYNTH_DESIGN.ARGS.DIRECTIVE RuntimeOptimized [get_runs synth_1]
+	set_property STEPS.SYNTH_DESIGN.ARGS.FSM_EXTRACTION off [get_runs synth_1]
+	set_property STEPS.WRITE_BITSTREAM.ARGS.READBACK_FILE 0 [get_runs impl_1]
+	set_property STEPS.WRITE_BITSTREAM.ARGS.VERBOSE 0 [get_runs impl_1]
 }
