f909813d0f4b56267e7547ede0f29c582f3d00e4
robert_bocos <robert-emilian.bocos@digilent.ro>
2024-07-19 10:56:35 +0300

    Upgraded hardware project to Vivado 2024.1.

--- project_info.tcl
+++ project_info.tcl
@@ -1,10 +1,11 @@
 # This is an automatically generated file used by digilent_vivado_checkout.tcl to set project options
 proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
-	set_property "part" "xc7z020clg484-1" $project_obj
-	set_property "default_lib" "xil_defaultlib" $project_obj
-	set_property "simulator_language" "Mixed" $project_obj
-	set_property "target_language" "VHDL" $project_obj
+    set_property "part" "xc7z020clg484-1" $project_obj
+    set_property "board_part" "" $project_obj
+    set_property "default_lib" "xil_defaultlib" $project_obj
+    set_property "simulator_language" "Mixed" $project_obj
+    set_property "target_language" "VHDL" $project_obj
 }
 
 proc set_project_properties_pre_add_repo {proj_name} {
@@ -14,5 +15,7 @@ proc set_project_properties_pre_add_repo {proj_name} {
 
 proc set_project_properties_post_create_runs {proj_name} {
     set project_obj [get_projects $proj_name]
-    # default nothing
+	#Custom directives for synthesis and implementation
+	set_property STEPS.WRITE_BITSTREAM.ARGS.READBACK_FILE 0 [get_runs impl_1]
+	set_property STEPS.WRITE_BITSTREAM.ARGS.VERBOSE 0 [get_runs impl_1]
 }
--- src/bd/design_1.tcl
+++ src/bd/design_1.tcl
@@ -20,12 +20,12 @@ set script_folder [_tcl::get_script_folder]
 ################################################################
 # Check if script is running in correct Vivado version.
 ################################################################
-set scripts_vivado_version 2023.1
+set scripts_vivado_version 2024.1
 set current_vivado_version [version -short]
 
 if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
    puts ""
-   common::send_gid_msg -ssname BD::TCL -id 2040 -severity "WARNING" "This script was generated using Vivado <$scripts_vivado_version> without IP versions in the create_bd_cell commands, but is now being run in <$current_vivado_version> of Vivado. There may have been major IP version changes between Vivado <$scripts_vivado_version> and <$current_vivado_version>, which could impact the parameter settings of the IPs."
+   common::send_gid_msg -ssname BD::TCL -id 2040 -severity "CRITICAL WARNING" "This script was generated using Vivado <$scripts_vivado_version> without IP versions in the create_bd_cell commands, but is now being run in <$current_vivado_version> of Vivado. There may have been changes to the IP between Vivado <$scripts_vivado_version> and <$current_vivado_version>, which could impact the functionality and configuration of the design."
 
 }
 
